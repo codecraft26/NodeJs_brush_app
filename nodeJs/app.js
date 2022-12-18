58748c7e4ca63ec0ae8d605ea6f5cc7b3dde4abf
codecraft26 <codescraft26@gmail.com>
2022-12-18 21:10:28 +0530

    adding json routes

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/json', (req, res) => {
+    res.json({
+        'name':"aman",
+        'deree':"b.tech"
+    })
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
